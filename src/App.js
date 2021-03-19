97a712495bffd369b8361c9d0f194b80b3b84eb0
Tiago880 <tiagocruz_880@hotmail.com>
2021-03-19 11:14:05 -0300

    Commit class 09 and 10 React Basico

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
 import React from "react";
 
+// * ---------------Aula 09 JSX do módulo 02------------------------* 
+//Essa eh a parte do código referente a 
+// aula 09 JSX do módulo 02 - React para iniciantes
+//
+// const App = () => {
+//   const livros = [
+//     {nome: 'A cabana', ano: 1996},
+//     {nome: 'O imitador', ano: 1998},
+//     {nome: 'Caminhando e cantando', ano: 2021},
+//   ];
+//   return (
+//     <ul>
+//       {livros.filter(({ano})=>ano >= 1998)
+//       .map(({nome, ano}) => (
+//       <li key={nome}>
+//         {nome},{ano}
+//       </li>
+//       ))}
+//     </ul>
+//   );  
+// };
+//*------------------------------------------------------------*
+
+//  *----------------Aula 10 JSX do módulo 02----------------------*
+const produtos = [
+  {
+  id: 1,
+  nome:'Smartphone',
+  preco: 'R$ 2000',
+  cores: ['#29d8d5' , '#252a34' , '#fc3766'],
+},
+{  
+  id: 2,
+  nome: 'Notebook',
+  preco: 'R$ 3000',
+  cores: ['#ffd045','#d4394b' , '#f37c59'],
+},
+{ id: 3,
+  nome:'Tablet',
+  preco:'R$ 1500',
+  cores:['#365069', '#47c1c8' , '#f95786'],
+},
+];
+
 const App = () => {
-  // return ( 
-  //   <>
-  //   <label htmlFor="nome">Nome</label>
-  //   <input type="text" id="nome"/>
-  //  </>
-  // );
-
-  const titulo = <h1>Esse é um titulo</h1>;
-  const random = Math.random();
-  const ativo = false;
-
-  const estiloP = {
-    color: 'blue',
-    fontSize: '2rem',
-  };
-
-
-
-  return  (
-  <>
-     {titulo}
-     <p style= {estiloP}>Estilização</p>
-    <p className={ativo ? "ativo" : "inativo"}>{(random * 1000) / 500}</p>
-  </>
-  );
+  const dados = produtos.filter(({preco})=> Number(preco.replace("R$",""))>1500);
+  console.log(dados);
+  return <section>
+    {dados.map(({id, nome , preco , cores}) =>(
+      <div key={id}>
+        <h1>{nome}</h1>
+        <p>Preço:{preco}</p>
+        <ul>{cores.map(cor => (
+          <li style={{backgroundColor: cor,color: 'white'}} key={cor}>{cor}</li>
+        ))}</ul>
+      </div>
+    ))}
+  
+  </section>;
 };
-
 export default App;

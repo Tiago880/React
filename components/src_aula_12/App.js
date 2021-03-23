f90c754f66624885ec1219dcaf211265ed83f023
Tiago880 <tiagocruz_880@hotmail.com>
2021-03-23 10:19:45 -0300

    add src_aula_13

---
+++ components/src_aula_12/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Form from './Form';
+
+
+
+
+const App =() => {
+
+  return ( 
+    <div>
+   
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  );
+};
+export default App;
